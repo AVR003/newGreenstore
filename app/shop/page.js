19d39decab3b52a669851387bcb6099639ec3caa
AVR003 <aakashvr93@gmail.com>
2025-10-15 17:53:00 +0530

    Fix Vercel build: make /shop page dynamic

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,5 +1,6 @@
 // src/app/shop/page.js
 'use client';
+export const dynamic = 'force-dynamic';
 import Image from 'next/image';
 import { useState, useMemo } from 'react';
 import { useCart } from '@/context/CartContext';
